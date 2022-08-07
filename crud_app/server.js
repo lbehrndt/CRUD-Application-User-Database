f088c482d7c5fee92c0238a87d8f651944de01e6
Leon <leonbehrndt1@gmail.com>
2022-08-07 03:41:43 +0200

    Creating html and ejs files
    Creating html and ejs files and implementing the basic page layout.

--- crud_app/server.js
+++ crud_app/server.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+
 const app = express();
 
-dotenv.config({path: "crud_app/views/config.env"})
-const PORT = process.env.PORT || 8080; 
+dotenv.config({ path: "crud_app/views/config.env" });
+const PORT = process.env.PORT || 8080;
 
 // log request
 app.use(morgan("tiny"));
 
-app.get("/", (req, res) => (
-    res.send("CRUD Application")
-))
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {console.log(`Server is running on http://localhost:${PORT}`)});
+app.set("view engine", "ejs");
+// app.set("views".path.resolve(__dirname: "views/ejs"));
+
+// load assets
+app.use(
+  "/css", express.static(path.resolve(__dirname, "assets/css"))
+);
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
